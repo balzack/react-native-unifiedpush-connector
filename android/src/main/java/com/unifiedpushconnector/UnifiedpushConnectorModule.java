4a2641ddff08fd8821ac9e01f44d14673a016d28
Roland Osborne <roland.osborne@gmail.com>
2023-06-12 16:16:12 -0700

    feat: connecting to unified push

--- android/src/main/java/com/unifiedpushconnector/UnifiedpushConnectorModule.java
+++ android/src/main/java/com/unifiedpushconnector/UnifiedpushConnectorModule.java
@@ -8,12 +8,22 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
 
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Context;
+import org.unifiedpush.android.connector.UnifiedPush;
+import org.unifiedpush.android.connector.RegistrationDialogContent;
+
 @ReactModule(name = UnifiedpushConnectorModule.NAME)
 public class UnifiedpushConnectorModule extends ReactContextBaseJavaModule {
   public static final String NAME = "UnifiedpushConnector";
-
+  private String packageName;
+  
   public UnifiedpushConnectorModule(ReactApplicationContext reactContext) {
     super(reactContext);
+
+    packageName = reactContext.getPackageName();
   }
 
   @Override
@@ -29,4 +39,22 @@ public class UnifiedpushConnectorModule extends ReactContextBaseJavaModule {
   public void multiply(double a, double b, Promise promise) {
     promise.resolve(a * b);
   }
+
+
+  @ReactMethod
+  public void initUnifiedPush(Promise promise) {
+
+    Activity activityContext = getCurrentActivity();
+
+    activityContext.getSharedPreferences("unifiedpush.connector", Context.MODE_PRIVATE).edit().putBoolean("unifiedpush.no_distrib_dialog", true).apply();
+
+    UnifiedPush.registerAppWithDialog(
+        activityContext,
+        "default",
+        new RegistrationDialogContent(),
+        new ArrayList<String>(),
+        packageName
+    );
+  }
+
 }
